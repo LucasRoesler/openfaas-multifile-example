a1d0c3e1b3c268f8f19d68791b3b02aa97b6c1fd
Lucas Roesler <roesler.lucas@gmail.com>
2019-01-19 20:02:54 +0100

    Add type hints
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- wordcloud/wordcloud.py
+++ wordcloud/wordcloud.py
@@ -1,26 +1,29 @@
+
 import unicodedata
 import os
+from typing import DefaultDict, Dict, List
 
 from operator import itemgetter
 from collections import defaultdict
 
 FILE = os.path.dirname(__file__)
 STOPWORDS = set(
-    map(str.strip, open(os.path.join(FILE, 'stopwords'), 'r').readlines())
+    map(str.lower,
+        map(str.strip,
+            open(os.path.join(FILE, 'stopwords'), 'r').readlines()
+            )
+        )
 )
 
 
-def process_text(text):
+def process_text(text: bytes) -> Dict[str, int]:
     """Splits a long text into words a count of interesting words in the text.
 
     process_text will elimate any of the stopwords, punctuation, and normalize
     the text to merge cases and plurals into a single value.
     """
-
-    stopwords = set([i.lower() for i in STOPWORDS])
     words = text.decode("utf-8").split()
     # remove stopwords
-    words = [word for word in words if word.lower() not in stopwords]
     # remove 's
     words = [
         word[:-2] if word.lower().endswith(("'s",))
@@ -30,17 +33,18 @@ def process_text(text):
     # remove numbers
     words = [word for word in words if not word.isdigit()]
     words = [strip_punctuation(word) for word in words]
+    words = [word for word in words if word.lower() not in STOPWORDS]
 
     return process_tokens(words)
 
 
-def process_tokens(words):
+def process_tokens(words: List[str]) -> Dict[str, int]:
     """Normalize cases and remove plurals.
     """
     # d is a dict of dicts.
     # Keys of d are word.lower(). Values are dicts
     # counting frequency of each capitalization
-    d = defaultdict(dict)
+    d: DefaultDict = defaultdict(dict)
     for word in words:
         word_lower = word.lower()
         # get dict of cases for word_lower
@@ -70,7 +74,7 @@ def process_tokens(words):
     return fused_cases
 
 
-def strip_punctuation(text):
+def strip_punctuation(text: str) -> str:
     punctutation_cats = set(['Pc', 'Pd', 'Ps', 'Pe', 'Pi', 'Pf', 'Po'])
     return ''.join(x for x in text
                    if unicodedata.category(x) not in punctutation_cats)
